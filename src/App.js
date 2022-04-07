c984e53328cfd02ec3cc1f6f85fc33c27ae0be07
Samardhh Reddy <samardhh@wedigit.tech>
2022-04-07 21:19:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
-
+let bluearray = []
 function App() {
+  const n = 4;
+  const [dum,setDum] = useState([])
+  const handleColor=(ele)=>{
+    bluearray.push(ele)
+    if(bluearray.length>2){
+      bluearray.shift();
+    }
+    setDum([...bluearray])
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center'}}>
+      <h2>Color Game</h2>
+      <table style={{width:'500px'}}>
+        <tbody>
+      {[...Array(n)].map((e, i) => <tr key={i} style={{height:'30px'}}>
+        {[...Array(n)].map((e, j) => <td key={j.toString()+i.toString()} style={{background:dum.includes(j.toString()+i.toString())?'red':'blue'}} onClick={()=>handleColor(j.toString()+i.toString())}></td>)}
+      </tr>)}
+      </tbody>
+      </table>
     </div>
   );
 }
